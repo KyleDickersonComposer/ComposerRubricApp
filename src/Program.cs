54f6c8d68af6c98ea440120a0fb991ea2b26cd32
KyleDickersonComposer <44070073+KyleDickersonComposer@users.noreply.github.com>
2023-12-25 12:36:55 -0500

    a basic working version.

--- src/Program.cs
+++ src/Program.cs
@@ -2,14 +2,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Hello Composer! What is your name?");
+        var cn = Console.ReadLine();
+        var pc = new ProsectiveComposer();
+
+        if (string.IsNullOrEmpty(cn))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid name.");
+            cn = Console.ReadLine();
+        }
+        else
+        {
+            pc.Name = cn;
+        }
+
         var rub = new Rubric();
         var cat = rub.Categories();
 
-        for (int i = 0; i < 9; i++)
+        for (int i = 0; i < cat.Count; i++)
         {
             Console.WriteLine(cat[i].CategoryName);
             Console.WriteLine(cat[i].CategoryInfo);
-            Console.ReadLine();
 
             for (int j = 0; j < cat[i].GradingInfo.Count; j++)
             {
@@ -17,7 +30,33 @@ public class Program
 
             }
 
-            Console.ReadLine();
+            Console.WriteLine("Evaluate yourself based on the above critera.");
+            Console.WriteLine("Please input your grade number.");
+
+            bool done = false;
+
+            while (!done)
+            {
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out int result) && result > 0 && result <= cat[i].GradingInfo.Count)
+                {
+                    pc.ratings.Add(cat[i].CategoryName, result);
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid grade number between 1 and {0}.", cat[i].GradingInfo.Count);
+                }
+            }
+        }
+
+        Console.WriteLine("Press enter to generate Report");
+        Console.ReadLine();
+
+        for (int i = 0; i < cat.Count; i++)
+        {
+            Console.WriteLine($"{cat[i].CategoryName} Grade: {pc.ratings[cat[i].CategoryName]}");
         }
     }
 }
--- src/ProsectiveComposer.cs
+++ src/ProsectiveComposer.cs
@@ -1,12 +1,5 @@
 public class ProsectiveComposer
 {
-    public int MusicalAbilityRating { get; set; }
-    public int CreditsNotorietyAssociationRating { get; set; }
-    public int TechnicalSkillRating { get; set; }
-    public int ArrangingOrchestrationRating { get; set; }
-    public int OnlinePresenceRating { get; set; }
-    public int SocialSkillsRating { get; set; }
-    public int OrganizationSkillsRating { get; set; }
-    public int ComputerProgrammingRating { get; set; }
-    public int IndustryKnowledgeRating { get; set; }
+    public string Name { get; set; } = "";
+    public Dictionary<string, int> ratings = new();
 }
