1ec3ad74af13bd72c83f726a91659ebbaa4ebfb5
KyleDickersonComposer <44070073+KyleDickersonComposer@users.noreply.github.com>
2023-12-26 12:55:31 -0500

    Update SocialSkillsData.cs

--- src/data/SocialSkillsData.cs
+++ src/data/SocialSkillsData.cs
@@ -14,7 +14,7 @@ public class SocialSkillsData : ICategoryData
 
         gi.Add("Grade 5 is a composer that is highly socially graceful speaks well without looking too self conscious or giving a bad vibe. This composer can introduce themselves and talk about their work in an engaging way. This composer isn't overbearing in trying to push their junk and is marketing quietly. This composer avoids the gross marketing tropes that are pushed on social media. And, they can engage with other people and seem interested in the work of others.");
         gi.Add("Grade 4 is a composer that fills the function of a grade 5 composer but does it with less grace. This composer might be slightly awkward or have a bad vibe. This composer might not be able to maintain peoples interest.");
-        gi.Add("Grade 3 is a composer that can't talk about their work and lacks social graces. This composer might be outright rude or inconsiderate. ");
+        gi.Add("Grade 3 is a composer that can't talk about their work and lacks social graces. This composer might be outright rude or inconsiderate.");
         gi.Add("Grade 2 is a composer that is extremely offputting and is unlikely to make any connections with civilized people.");
         gi.Add("Grade 1... yup...");
     }
